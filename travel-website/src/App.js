d0aace0bb61e0d69da0f756ad29368d94ab3fd83
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-21 08:34:54 +0800

    Finished Home Page

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/services" Component={Services} />
+          <Route path="/products" Component={Products} />
+          <Route path="/sign-up" Component={SignUp} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
